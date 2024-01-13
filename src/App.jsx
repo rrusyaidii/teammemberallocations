b5834251d325ae504695e617c7ab897d6dd191c3
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2024-01-13 14:20:17 +0800

    Next commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
